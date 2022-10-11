b8020b5806c125d45e8fd04c5352c48f71072c2f
andrey352 <bauer350@mail.ru>
2022-10-11 15:15:01 +0300

    fixed .gitignore.

--- task/Program.cs
+++ task/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив строк, длина которых меньше
+// либо равна 3 символам. Первоначальный массив можно ввести с клав-ы, либо задать на старте выполнения алгоритма
+// Пример: ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+
+string[] array1 = {"hello", "2", "world", ":-)"};
+string[] newArray = new string[array1.Length];
+
+void GetResultArray (string[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+GetResultArray(array1);
+PrintArray(newArray);
